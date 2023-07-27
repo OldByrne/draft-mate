2e8455a70c09f2afac1154f4f18ff2a4fe55acdb
David <davidayresbyrne@gmail.com>
2023-07-27 13:02:08 +0100

    first commit of backend app and text files

---
+++ draft-mate-backend/src/main/java/com/davidbyrne/draftmate/DraftMateApplication.java
@@ -0,0 +1,13 @@
+package com.davidbyrne.draftmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftMateApplication.class, args);
+	}
+
+}
---
+++ draft-mate-backend/src/test/java/com/davidbyrne/draftmate/DraftMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidbyrne.draftmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
